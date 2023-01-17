00a1451e7cf0ffa7f221db2da3e59667b691d9b9
armaan-crypto <armaanahmed08@gmail.com>
2023-01-16 19:17:24 -0500

    Initial Commit

---
+++ Comfort Rides Pro/Comfort_Rides_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comfort_Rides_ProApp.swift
+//  Comfort Rides Pro
+//
+//  Created by Armaan Ahmed on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Comfort_Rides_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comfort Rides Pro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Comfort Rides Pro
+//
+//  Created by Armaan Ahmed on 1/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
